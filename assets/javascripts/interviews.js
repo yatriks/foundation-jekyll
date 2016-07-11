54d53fcea3c4349565cd38f31d465b8524f0b1e4
Yatrik Solanki <yatrik@architizer.com>
2016-07-11 18:48:41 -0400

    Add grid randomization again.

--- assets/javascripts/interviews.js
+++ assets/javascripts/interviews.js
@@ -13,15 +13,15 @@ function randomIntFromInterval(min,max)
 
 jQuery(document).ready(function($) {
 
-  // $.each($(".interview"), function (i, val) {
-  //   if ($( window ).width() > "640") {
-  //     $(this).css("margin-top",randomIntFromInterval(-20, 0)+"px");
-  //     $(this).parent().css("margin-left",randomIntFromInterval(-20, 20)+"px");
-  //   } else if ($( window ).width() <= "640") {
-  //     $(this).parent().css("margin-left",randomIntFromInterval(-20, 20)+"px");
-  //     $(this).parent(":not(:first-child)").css("margin-top",randomIntFromInterval(-50, -20)+"px");
-  //   }
-  // });
+  $.each($(".interview"), function (i, val) {
+    if ($( window ).width() > "640") {
+      $(this).css("margin-top",randomIntFromInterval(-20, 0)+"px");
+      $(this).parent().css("margin-left",randomIntFromInterval(-20, 20)+"px");
+    } else if ($( window ).width() <= "640") {
+      $(this).parent().css("margin-left",randomIntFromInterval(-20, 20)+"px");
+      $(this).parent(":not(:first-child)").css("margin-top",randomIntFromInterval(-50, -20)+"px");
+    }
+  });
 
   // Hover for interviews
   $(".interview").hover(function() {
