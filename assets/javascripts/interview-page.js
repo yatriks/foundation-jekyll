72aaefeda53afc1eb76e4616583435942dc0a67a
Yatrik Solanki <yatrik@architizer.com>
2016-07-12 00:48:03 -0400

    Change image loading logic to be better with responsive/adapt to image orientation.

--- assets/javascripts/interview-page.js
+++ assets/javascripts/interview-page.js
@@ -9,19 +9,37 @@ $(document).foundation();
 jQuery(document).ready(function($) {
 
   $.each($('.interview-text > p'), function(i, val) {
-    $(this).attr("data-para-index",i);
-    if(!$(this).has("img").length) {
+    $(this).attr("data-para-index", i);
+    if (!$(this).has("img").length) {
       $(this).addClass("end");
     } else {
       var para = $(this);
       var image = $(this).children("img").attr("src");
       var leftOffset = para.offset().left + para.width() + 30;
-      para.after("<div class='medium-2 large-4' style='position: absolute; left: "+ leftOffset +"px ;top:"+para.offset().top+"px;'><img src='"+image+"' /></div>");
+      para.wrap("<span class='with-image'></span>");
+      para.after("<div class='small-12 medium-5 large-4 columns text-center end'><img class='inline-image' id='image-" + i + "' src='" + image + "' /></div>");
+      para.children("img").remove();
+      // para.height($("#image-"+i).height());
       // console.log("hi");
       // $(this).after('<div class="hide-for-small medium-2 large-5 columns" style="border:1px solid red;">hi</div>');
     }
   });
-  
+
+  $(window).resize(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth > 480) {
+      $.each($('.with-image'), function(i, val) {
+        var para = $(this).find("p");
+        var image = $(this).find(".inline-image");
+        var imageHeight = image.height() + "px";
+        var paraHeight = para.height() + "px";
+        var chosenHeight = Math.max(imageHeight, paraHeight);
+        para.height(chosenHeight);
+        image.height(chosenHeight);
+      });
+    }
+  });
+
 });
 
 // Init Highlight.js
