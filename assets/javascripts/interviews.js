c5602b49fa94c03e8fd9745c8f477a774ca94c00
Yatrik Solanki <yatrik@architizer.com>
2016-07-12 00:48:27 -0400

    Add filter and tag-sort options on the interviews view.

--- assets/javascripts/interviews.js
+++ assets/javascripts/interviews.js
@@ -1,35 +1,89 @@
-//
-// Foundation-Jekyll - Javascripts
-// Author: @aaronkwhite
-//
-
-// Init Foundation
 $(document).foundation();
 
-function randomIntFromInterval(min,max)
-{
-    return (Math.random()*(max-min+1)+min);
+function randomIntFromInterval(min, max) {
+  return (Math.random() * (max - min + 1) + min);
 }
+
+function toggleArrayItem(array, value) {
+    var i = array.indexOf(value);
+    if (i === -1)
+        array.push(value);
+    else
+        array.splice(i,1);
+}
+
+var findOne = function (haystack, arr) {
+    if (arr.length === 0) {
+      return true;
+    }
+    return arr.some(function (v) {
+        return haystack.indexOf(v) >= 0;
+    });
+};
 
 jQuery(document).ready(function($) {
 
-  $.each($(".interview"), function (i, val) {
-    if ($( window ).width() > "640") {
-      $(this).css("margin-top",randomIntFromInterval(-20, 0)+"px");
-      $(this).parent().css("margin-left",randomIntFromInterval(-20, 20)+"px");
-    } else if ($( window ).width() <= "640") {
-      $(this).parent().css("margin-left",randomIntFromInterval(-20, 20)+"px");
-      $(this).parent(":not(:first-child)").css("margin-top",randomIntFromInterval(-50, -20)+"px");
+  $.each($(".interview"), function(i, val) {
+    if ($(window).width() > "640") {
+      $(this).css("margin-top", randomIntFromInterval(-20, 0) + "px");
+      $(this).parent().css("margin-left", randomIntFromInterval(-20, 20) + "px");
+    } else if ($(window).width() <= "640") {
+      $(this).parent().css("margin-left", randomIntFromInterval(-20, 20) + "px");
+      $(this).parent(":not(:first-child)").css("margin-top", randomIntFromInterval(-50, -20) + "px");
     }
   });
 
   // Hover for interviews
   $(".interview").hover(function() {
+
     $(this).parent().siblings().toggleClass("unfocused");
-    if ($( window ).width() > "640") {
-        $(this).siblings().toggleClass("visible");
-      }
+    if ($(window).width() > "640") {
+      $(this).siblings().toggleClass("visible");
+    }
   });
+
+  // Type to filter
+  $('#filter').keyup(function() {
+
+    console.log("keyup!");
+
+    var filter = this.value.toLowerCase(); // no need to call jQuery here
+
+    $('.interview-container').each(function() {
+
+      var interview = $(this);
+      var title = interview.find('a.interview > p').text().toLowerCase();
+
+      // console.log(title, title.indexOf(filter), filter);
+
+      if (title.indexOf(filter) < 0) {
+        interview.hide();
+      } else if (title.indexOf(filter) >= 0) {
+        interview.show();
+      }
+
+    });
+  });
+
+  var selected = []; // selected filters
+
+  $(".tag.filter").click (function() {
+    var slug = $(this).attr("data-filter-tag");
+    toggleArrayItem(selected, slug);
+    $(this).toggleClass("selected");
+    console.log(selected);
+    $('.interview-container').each(function() {
+      this_tags = $(this).attr("data-filter-tags").split(",");
+      var found = findOne(this_tags, selected);
+      console.log($(this).children(".interview").text().trim()+": "+found);
+      if (found === true) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+
 });
 
 // Init Highlight.js
